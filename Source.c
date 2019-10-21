1bab8f46722f83a4e75a429470bb9299a0d25d6e
kornelijusp <kornelijus.petronis2000@gmail.com>
2019-10-21 21:11:53 +0300

    v2.0

--- Source.c
+++ Source.c
@@ -1,6 +1,55 @@
+/************************
+ *                      *
+ *  Kornelijus Petronis *
+ *      namu darbas     *
+ *   Assessment task 1  *
+ *        EAf-19        *
+ *                      *
+ ***********************/
+
+// Library list
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-void main() {
-printf("Test\n");
+// =========================================
+// Operation list
+char* operation[15] = {
+    "floor",
+    "round",
+    "ceil",
+    "sin",
+    "cos",
+    "cosh",
+    "exp",
+    "tan",
+    "tanh",
+    "sinh",
+    "log",
+    "log10",
+    "sqrt",
+    "pow",
+    "trunc"};
+// =========================================
+int main(int argc, char *argv[])
+{
+    int number;
+    char oper[10];
 
+    number = atoi(argv[2]);
+
+    
+    for (int i = 0; i < 15; i++)
+    {
+        if (strcmp(operation[i], argv[1]) == 0)
+           printf("%d\n", i);
+    }
+
+    // printf("%s\n", operation);
+    printf("%s ", argv[1]);
+    printf("%d\n", number);
+
+    // printf("Operation: %d\n", operation);
+
+    return 0;
 }
